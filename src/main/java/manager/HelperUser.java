306f0647a5047acafc644ec72fa38796f5bc6f63
marushana <marushana@yandex.ru>
2026-05-04 17:20:37 +0300

    login success test method getMessage method pause

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,47 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+
+    public void openLoginForm() {
+//        WebElement loginTab = wd.findElement(By.xpath("//a[text() = 'LOGIN']"));
+//        loginTab.click();
+        click(By.xpath("//a[text() = ' Log in ']"));
+    }
+
+    public void openRegistrationForm() {
+//        WebElement loginTab = wd.findElement(By.xpath("//a[text() = 'LOGIN']"));
+//        loginTab.click();
+        click(By.xpath("//a[text() = ' Sign up ']"));
+    }
+    public void fillLoginRegistrationForm(String email, String password){
+//        WebElement emailInput = wd.findElement(By.xpath("//*[text() = 'Email ']"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys(email);
+        type(By.xpath("//input[@id = 'email']"), email);
+//        WebElement passwordInput = wd.findElement(By.xpath("//*[text() = 'Password ']"));
+//        passwordInput.click();
+//        passwordInput.clear();
+//        passwordInput.sendKeys(password);
+        type(By.xpath("//input[@id = 'password']"), password);
+    }
+
+    public void submitLogin(){
+        click(By.xpath("//*[text() = 'Y’alla!']"));
+    }
+
+    public String getMessage(){
+        WebElement element = wd.findElement(By.xpath("//h2[@class = 'message']"));
+        return element.getText();
+    }
+
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,10 +1,21 @@
 package tests;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
     @Test
     public void test(){
 
+    }
+
+    @Test
+    public void loginSuccess(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginRegistrationForm("marushana@yandex.ru", "Pokrov13041986!");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
     }
 }
