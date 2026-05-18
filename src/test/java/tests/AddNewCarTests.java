050aefd333ed185757de03c4d08114469e26a33e
marushana <marushana@yandex.ru>
2026-05-18 22:50:54 +0300

    addCar with js and attach photo

--- src/test/java/tests/AddNewCarTests.java
+++ src/test/java/tests/AddNewCarTests.java
@@ -31,6 +31,7 @@ public class AddNewCarTests extends TestBase{
                 .about("Nice car")
                 .build();
 
+
         app.getHelperCar().openCarForm();
         app.getHelperCar().fillCarForm(car);
         app.getHelperCar().attachPhoto("C:\\Users\\marus\\Qa33\\QA_33_51_IlCarro\\Bugatti_Veyron_16.4_–_Frontansicht_(1),_5._April_2012,_Düsseldorf 2.jpg");
